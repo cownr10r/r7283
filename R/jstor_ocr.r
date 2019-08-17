e3923e8800da3cd51c2499c6fac8d3f87f8d86cb
Mario Martinez <mario.a.martinez@live.com>
2019-08-17 09:38:32 -0500

    cleaning house and removing old stuff.

--- R/jstor_ocr.r
+++ R/jstor_ocr.r
@@ -1,10 +1,10 @@
 #' Take metadata and ocr text from D4R and merge it into xml files.
 #'
-#' @param a The path to the unzipped Jstor folder.
-#' @param b The path to a folder that will contain the output files.
+#' @param a1 The path to the unzipped Jstor folder.
+#' @param b1 The path to a folder that will contain the output files.
 #' @return xml files with merged metadata and machine readable text.
 #' @examples jstor_ocr(a = "path/to/d4r/files", b = "destination/folder/path")
-jstor_ocr <- function(a = input_path, b = output_path){
+jstor_ocr <- function(a1 = input_path, b1 = output_path){
 
   hi <- list.files(paste0(a, "/metadata"), full.names=T)
   ho <- list.files(paste0(a, "/ocr"), full.names = T)
@@ -29,10 +29,10 @@ jstor_ocr <- function(a = input_path, b = output_path){
   for(i in seq_along(xmldata)){
     a <- xmldata[[i]]
     b <- textdata[[i]]
-    d <- xml2::xml_add_child(a, b) # I think the problem is somehwere here, but I'm not sure.
+    d <- xml2::xml_add_child(a, b) # The problem is somehwere here.
     e <- hi[[i]]
     f <- paste(e, ".xml")
-    xml2::write_xml(a, file = paste("C:/Users/mariomartinez/Desktop/snake/", f))
+    xml2::write_xml(a, file = paste(b1, f))
   }
 
 }
