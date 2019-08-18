2b41c0462bcb09703971d1ac4b9f787f35a190f8
Mario Martinez <mario.a.martinez@live.com>
2019-08-18 04:52:39 -0500

    Creating jstor_ocr function with other documentation.

--- R/jstor_ocr.r
+++ R/jstor_ocr.r
@@ -2,21 +2,46 @@
 #'
 #' @param a1 The path to the unzipped Jstor folder.
 #' @param b1 The path to a folder that will contain the output files.
-#' @return xml files with merged metadata and machine readable text. Quality control message indicating parsimony between indexed file names.
+#' @return xml files with merged metadata and machine readable text. Quality control message
+#' @return indicating parsimony between indexed file names.
+#' @return currently will throw warning messages due to xml2.
 #' @examples jstor_ocr(a1 = "path/to/d4r/files", b1 = "destination/folder/path")
 jstor_ocr <- function(a1 = input_path, b1 = output_path){
 
   # read in files from unzipped master folder
 
-  hi <- list.files(paste0(a, "/metadata"), full.names=T)
-  ho <- list.files(paste0(a, "/ocr"), full.names = T)
+  hi <- list.files(paste0(a1, "/metadata"), full.names=T)
+  ho <- list.files(paste0(a1, "/ocr"), full.names = T)
 
   # read in files to a list
 
   xmldata <- lapply(hi, xml2::read_xml)
-  textdata <- lapply(ho, xml2::read_xml)
 
-  # cleanse names for xml
+  writing <- lapply(ho, readLines, warn = FALSE)
+
+  # Remove page sequence designations, dashes, and whitespace between dashes and dislocated syllables
+  # gsub out <plain_text> tags for your own <text> tags.
+
+ #  for(i in seq_along(text)){
+ #   text[[i]] <- iconv(text[[i]], "UTF-8", "UTF-8",sub='')
+  #  text[[i]] <- gsub("</page>", "", text[[i]])
+  #  text[[i]] <- gsub("<page sequence=\".\">", "", text[[i]])
+  #  text[[i]] <- gsub("-\\s+|\\s+-\\s+|\\s+- |-" ,"", text[[i]])
+   # text[[i]] <- tolower(text[[i]])
+ #  }
+
+  for(i in seq_along(writing)){
+    writing[[i]] <- iconv(writing[[i]], "UTF-8", "UTF-8", sub='')
+    writing[[i]] <- gsub("<.{4,20}>", "", writing[[i]])
+    writing[[i]] <- gsub("^", "<text>", writing[[i]])
+    writing[[i]] <- gsub("$", "</text>", writing[[i]])
+    writing[[i]] <- gsub("-\\s+|\\s+-\\s+|\\s+- |-" ,"", writing[[i]])
+    writing[[i]] <- tolower(writing[[i]])
+  }
+
+  textdata <- lapply(writing, xml2::read_xml)
+
+  # cleanse names for .xml
 
   for(i in seq_along(hi)){
     hi[[i]] <- gsub(".*-", "", hi[[i]])
@@ -30,12 +55,15 @@ jstor_ocr <- function(a1 = input_path, b1 = output_path){
     ho[[i]] <- gsub(".txt","", ho[[i]])
   }
 
+
+
   for(i in seq_along(xmldata)){
     a <- xmldata[[i]]
     b <- textdata[[i]]
     d <- xml2::xml_add_child(a, b)
     e <- hi[[i]]
-    f <- paste(e, ".xml")
+    f <- paste0("/", e)
+    f <- paste0(f, ".xml")
     xml2::write_xml(a, file = paste(b1, f))
   }
 
---
+++ R/neuter_spanish.r
@@ -0,0 +1,16 @@
+#' Take metadata and ocr text from D4R and merge it into xml files.
+#'
+#' @param x  A character vector with Spanish in it. At the moment has not been tested on ascii files or latin encoding.
+#' @return Neutered nouns and adjectives in Spanish. This package function was developed for a particular purpose, and
+#' @return the project goal should be closely considered before implementing. The function is currently in beta testing.
+#' @examples
+neuter_spanish <- function(x){
+  x <- unlist(strsplit(x, " "))
+  x <- tolower(x)
+  x <- gsub("[[:punct:]]", "", x)
+  x <- gsub("o$", "x", x)
+  x <- gsub("a$", "x", x)
+  x <- gsub("as$", "xs", x)
+  x <- gsub("os$", "xs", x)
+  x <- paste(x, collapse = " ")
+}
